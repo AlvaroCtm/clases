169803261e6e07df3a8514459a2d9a67c6491acf
alvaro <alvaro101_1@hotmail.com>
2021-06-24 17:21:41 -0500

    clases

---
+++ ejemplosdeclases/src/ejemplosdeclases/estudiante.java
@@ -0,0 +1,30 @@
+package ejemplosdeclases;
+
+public class estudiante {
+	// encapulamiento//
+	private String nombre;
+	private int matricula;
+
+	// abstraccion//
+
+	void  matricularse (String hora , String dias){
+	int dia = 2 ;
+	String valor = hora + dias ;
+	}
+	
+	//polimorfismo //
+	//instaciar de una clase padre a hija//
+	
+	 void profesor(String carlos) {
+		int edad = 31;
+		String nombre = carlos;
+		String quimica = carlos; 
+		
+	}
+		
+	
+		
+		
+	
+
+}
---
+++ ejemplosdeclases/src/module-info.java
@@ -0,0 +1,2 @@
+module ejemplosdeclases {
+}
